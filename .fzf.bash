cb979d996d99287112f90c642a7c4bf27525d06f
Andrei Maksimenka <amaksimenka@amaksimenka-linux.sea.corp.google.com>
2017-02-03 16:03:54 -0800

    update

--- .fzf.bash
+++ .fzf.bash
@@ -4,12 +4,6 @@ if [[ ! "$PATH" == */usr/local/google/home/amaksimenka/.fzf/bin* ]]; then
   export PATH="$PATH:/usr/local/google/home/amaksimenka/.fzf/bin"
 fi
 
-# Man path
-# --------
-if [[ ! "$MANPATH" == */usr/local/google/home/amaksimenka/.fzf/man* && -d "/usr/local/google/home/amaksimenka/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/google/home/amaksimenka/.fzf/man"
-fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/usr/local/google/home/amaksimenka/.fzf/shell/completion.bash" 2> /dev/null
@@ -19,3 +13,6 @@ fi
 source "/usr/local/google/home/amaksimenka/.fzf/shell/key-bindings.bash"
 
 source ~/fzf-extras.sh
+
+# Setting ag as the default source for fzf
+export FZF_DEFAULT_COMMAND='ag -g ""'
